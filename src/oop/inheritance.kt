b4e4b852e0f8e73beaf3890ba56f28bb9e2f4e01
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-08-23 14:15:47 +0300

    add: inheritance example
    Took 35 minutes

---
+++ src/oop/inheritance.kt
@@ -0,0 +1,19 @@
+package oop
+
+//by default classes in Kotlin are final, if you want to allow inheritance? mark them with <open> modifier
+open class Bird(var name: String) {
+    fun fly() {
+        println("$name is flying!")
+    }
+}
+
+// You can inherit class in two ways:
+//first is calling primary constructor of parental class just after class declaration
+class Duck(name: String) : Bird(name)
+/*second one is calling parental class constructor using super
+class Duck: Bird{
+    constructor(name: String): super(name){
+        
+    }
+}
+*/
